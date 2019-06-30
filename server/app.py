4fcb586b378f18df979529501714756fd43e33b5
EvgenyKashin <kashinevge@gmail.com>
2019-06-30 17:18:51 +0300

    Add displaying of random 9 images

--- server/app.py
+++ server/app.py
@@ -1,13 +1,21 @@
+from pathlib import Path
+import random
 from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__, template_folder='static')
+images_path = Path('server/images')
+all_images = list(images_path.iterdir())
 
 
 @app.route('/')
 def index():
+    random.shuffle(all_images)
     return render_template('index.html')
 
 
 @app.route('/images/<filename>')
 def serve_image(filename):
-    return send_from_directory('images', filename, cache_timeout=-1)
+    position = int(filename.split('.')[0].split('_')[1])
+    return send_from_directory(images_path.name,
+                               all_images[position].name,
+                               cache_timeout=-1)
