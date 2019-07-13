bfe95231c7a079364ef15f48dc9ed21d28104316
EvgenyKashin <kashinevge@gmail.com>
2019-07-13 21:13:21 +0300

    Add GA events tracking

--- server/static/main.js
+++ server/static/main.js
@@ -1,3 +1,27 @@
 document.addEventListener('DOMContentLoaded', e => {
+    const yaButton = document.querySelector('.btn-success');
+    yaButton.addEventListener('click', e => {
+        gtag('event', 'yandex_donate_click', {
+            'event_category': 'donate'
+        });
+    });
+
+    const paypalButton = document.querySelector('.btn-info');
+    paypalButton.addEventListener('click', e => {
+        gtag('event', 'paypal_donate_click', {
+            'event_category': 'donate'
+        });
+    });
+
+    const cards = document.querySelectorAll('.card-body');
+    cards.forEach(function (card, i, arr) {
+        card.addEventListener('click', e => {
+            let imgUrl = card.parentNode.href;
+            gtag('event', 'img_click', {
+                'event_category': 'downloads',
+                'event_label': imgUrl
+            });
+        });
+    });
 
 });
