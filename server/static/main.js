c63f22758611afdba6f6d4c900853287669b991e
EvgenyKashin <kashinevge@gmail.com>
2019-07-15 09:42:02 +0300

    Add like button click processing

--- server/static/main.js
+++ server/static/main.js
@@ -13,6 +13,55 @@ document.addEventListener('DOMContentLoaded', e => {
         });
     });
 
+    const twitterButton = document.querySelector('li:nth-child(1)');
+    twitterButton.addEventListener('click', e => {
+        gtag('event', 'twitter_like_click', {
+            'event_category': 'likes'
+        });
+    });
+
+    const fbButton = document.querySelector('li:nth-child(2)');
+    fbButton.addEventListener('click', e => {
+        gtag('event', 'fb_like_click', {
+            'event_category': 'likes'
+        });
+    });
+
+    const githubButton = document.querySelector('li:nth-child(3)');
+    githubButton.addEventListener('click', e => {
+        gtag('event', 'github_like_click', {
+            'event_category': 'likes'
+        });
+    });
+
+    const donateNavigationButton = document.querySelector('li:nth-child(4)');
+    donateNavigationButton.addEventListener('click', e => {
+        gtag('event', 'donate_navigation_click', {
+            'event_category': 'donate'
+        });
+    });
+
+    const moreInfoButton = document.querySelector('.navbar-toggler');
+    moreInfoButton.addEventListener('click', e => {
+        gtag('event', 'more_info_click', {
+            'event_category': 'info'
+        });
+    });
+
+    const styleganButton = document.querySelector('.text-muted a:nth-child(1)');
+    styleganButton.addEventListener('click', e => {
+        gtag('event', 'stylegan_click', {
+            'event_category': 'info'
+        });
+    });
+
+    const superResButton = document.querySelector('.text-muted a:nth-child(2)');
+    superResButton.addEventListener('click', e => {
+        gtag('event', 'super_res_click', {
+            'event_category': 'info'
+        });
+    });
+
     const cards = document.querySelectorAll('.card-body');
     cards.forEach(function (card, i, arr) {
         card.addEventListener('click', e => {
