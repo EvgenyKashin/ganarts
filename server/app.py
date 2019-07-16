e6294b4cd4efd8e1e66a43d996e949675354d5c2
EvgenyKashin <kashinevge@gmail.com>
2019-07-16 09:07:50 +0300

    Add work in progress base64 version

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import json
 from io import BytesIO
+import base64
 from flask import Flask, render_template, send_file
 import redis
 
@@ -25,16 +26,34 @@ def load_image(filename):
 def index():
     sync_file.touch()
     urls = read_urls()
+    t_shirt_images, images = read_imgs()
+    urls = zip(urls, t_shirt_images, images)
     return render_template('index.html',
                            urls=urls)
 
 
-@app.route('/images/<filename>.png')
-def serve_image(filename):
+# @app.route('/images/<filename>.png')
+# def serve_image(filename):
+#     img = load_image(filename)
+#     return send_file(BytesIO(img),
+#                      mimetype='image/png',
+#                      cache_timeout=-1)
+
+def get_image(filename):
     img = load_image(filename)
-    return send_file(BytesIO(img),
-                     mimetype='image/png',
-                     cache_timeout=-1)
+    # img = BytesIO(img).get_value().
+    # img = img.encode('base64')
+    img = base64.b64encode(img)
+    return img
+
+
+def read_imgs():
+    t_shirt_image_names = [f't_shirt_image_{i}' for i in range(9)]
+    image_names = [f'image_{i}' for i in range(9)]
+
+    t_shirt_images = [get_image(name) for name in t_shirt_image_names]
+    images = [get_image(name) for name in image_names]
+    return t_shirt_images, images
 
 
 if __name__ == '__main__':
